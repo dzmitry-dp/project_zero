093c6061633062ca741dee622994e562e750467f
Dzmitry <dzmitrupaulenka@outlook.com>
2020-11-16 19:23:31 +0300

    First dev commit

--- module_0/gtn.py
+++ module_0/gtn.py
@@ -11,6 +11,20 @@ def game_core_v1(number):
         if number == predict: 
             return(count) # выход из цикла, если угадали
         
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1,101)
+    while number != predict:
+        count+=1
+        if number > predict: 
+            predict += 1
+        elif number < predict: 
+            predict -= 1
+    return(count) # выход из цикла, если угадали
+
         
 def score_game(game_core):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
@@ -24,4 +38,4 @@ def score_game(game_core):
     return(score)
 
 
-score_game(game_core_v1)
+score_game(game_core_v2)
