3a1c29e43e76a54ef4ce2b8b272aac31cd92a0fc
Dzmitry <dzmitrupaulenka@outlook.com>
2021-04-03 00:39:33 +0300

    Best result 0.25

--- module_3/handling.py
+++ module_3/handling.py
@@ -21,56 +21,94 @@ def format_str_values_to_list(text):
 
 class ConfigWrapper:
     def __init__(self, data):
-        self.raw_data = data
+        self.raw_data = data.copy()
         self.final_data = self.raw_data.copy()
-        # удаляю не нужные для модели признаки
-        self.final_data.drop(['Restaurant_id','ID_TA',], axis = 1, inplace=True)
+        # зафиксирую где отсутствует информация
+        self.final_data['Cuisine_Style_isNAN'] = pd.isna(self.raw_data['Cuisine Style']).astype('int64')
+        self.final_data['Price_Range_isNAN'] = pd.isna(self.raw_data['Price Range']).astype('int64')
+        self.final_data['Reviews_isNAN'] = pd.isna(self.raw_data['Reviews']).astype('int64')
         # обрабатываю каждый признак
-        self.processing_city()
+        self.processing_restaurant_id()
         self.processing_cuisine_style()
         self.processing_ranking()
         self.processing_price_range()
-        self.processing_number_of_reviews()
         self.processing_reviews()
+        self.processing_city() # после cuisine style и price range, т.к. заполняем средним по городам
+        self.processing_number_of_reviews()
         self.processing_url_ta()
+        self.processing_id_ta() # первым обрабатываю признак ID_TA т.к. есть дублирующая информация
+
+    def processing_restaurant_id(self):
+        # приводим id к числовому формату
+        self.final_data['Restaurant_id'] = self.raw_data['Restaurant_id'].apply(lambda x: int(x.replace('id_', '')))
+        # выделяем отдельным признаком средний рейтинг сети ресторанов
+        self.final_data['Average_Rating_Series_Restaurant'] = self.final_data['Restaurant_id'].apply(lambda x: round(self.final_data.Rating[self.final_data.Restaurant_id == x].mean(), 1))
+        # удаляю столбец Restaurant_id
+        # self.final_data = self.final_data.drop(columns='Restaurant_id')
 
     def processing_city(self):
-        self.final_data = pd.get_dummies(self.final_data, columns=['City'], dummy_na=True)
+        # big_cities = ['London', 'Paris', 'Madrid', 'Barcelona', 'Berlin', 'Milan']
+        big_cities_data = pd.get_dummies(self.final_data[
+                                            (self.final_data['City'] == 'London') |\
+                                            (self.final_data['City'] == 'Paris') |\
+                                            (self.final_data['City'] == 'Madrid') |\
+                                            (self.final_data['City'] == 'Barcelona') |\
+                                            (self.final_data['City'] == 'Berlin') |\
+                                            (self.final_data['City'] == 'Milan')
+                                            ], columns=['City'], dummy_na=True, dtype='int64')
+        self.final_data = self.final_data.merge(big_cities_data, how='left') 
+        # удаляем дубликаты
+        self.final_data.drop_duplicates(inplace=True)
+        # представители малых городов 0
+        self.final_data['City_London'].fillna(0, inplace=True)
+        self.final_data['City_Paris'].fillna(0, inplace=True)
+        self.final_data['City_Madrid'].fillna(0, inplace=True)
+        self.final_data['City_Barcelona'].fillna(0, inplace=True)
+        self.final_data['City_Berlin'].fillna(0, inplace=True)
+        self.final_data['City_Milan'].fillna(0, inplace=True)
+        # рестораны в не больших городах 1
+        self.final_data['City_nan'].fillna(1, inplace=True)
+        # удаляю столбец City
+        self.final_data = self.final_data.drop(columns='City')
 
     def processing_cuisine_style(self):
-        # зафиксирую где отсутствует информации в колонке Cuisine Style
-        self.final_data['Cuisine_Style_isNAN'] = pd.isna(self.final_data['Cuisine Style']).astype('uint8')
         # форматируем данные в столбце
         self.final_data['Cuisine Style'] = self.final_data['Cuisine Style']\
                                                 .apply(format_str_values_to_list)
-        
-        # создаем список из уникальных стилей, присутствующих в датафрейме
+        # копирую столбец для удобства обращения
+        self.raw_data['Cuisine Style'] = self.final_data['Cuisine Style'].copy()
+        # создаю столбец "Количество кухонь в одном ресторане"
+        self.final_data['Amount_Cuisine_Style'] = self.final_data['Cuisine Style'].str.len()
+        # заполняем пропуски
+        for idx in self.final_data[self.final_data['Amount_Cuisine_Style'].isnull()].index:
+            self.final_data.at[idx, 'Amount_Cuisine_Style'] = round(self.final_data['Amount_Cuisine_Style'][self.final_data['City'] == self.final_data['City'].iloc[idx]].mean())
+ 
+        # создаем список из уникальных стилей кухонь, присутствующих в датафрейме
         unique_cuisine_style = CuisineStyle.get_unique_values_cuisine_style(self.final_data['Cuisine Style'])
-
         # добавляем колонки для каждой кухни
         for elem in unique_cuisine_style:
             self.final_data[elem] = pd.Series(np.zeros(len(self.raw_data.index), dtype=int))
-
+        # # # заполняем пропуски Cuisine Style самой популярной кухней в городе
+        # # # nan_series = self.raw_data[self.raw_data['Cuisine Style'].isnull()]
+        # # # for idx in nan_series.index:
+        # # #     self.final_data.at[idx, 'Cuisine Style'] = CuisineStyle.get_most_popular_cuisine(self.raw_data['Cuisine Style'][self.raw_data['City'] == self.raw_data.iloc[idx]['City']])
         # раставляем 1 в соответствующей колонке кухни
         explode_series = self.final_data['Cuisine Style'].explode()
         for cuisine in unique_cuisine_style:
             for index_ in explode_series.loc[explode_series.isin([cuisine])].index.values:
                 self.final_data.at[index_, cuisine] = 1
 
-        # создаю столбец "Количество кухонь в одном ресторане"
-        self.final_data['Amount_Cuisine_Style'] = self.final_data['Cuisine Style'].str.len()
-
         # удаляю столбец Cuisine Style
         self.final_data = self.final_data.drop(columns='Cuisine Style')
-        # заполняем пропуски
-        self.final_data['Amount_Cuisine_Style'].fillna(self.final_data['Amount_Cuisine_Style'].mean(), inplace=True)
 
     def processing_ranking(self):
+        # удаляю столбец Ranking
+        # self.final_data = self.final_data.drop(columns='Ranking')
         pass
 
     def processing_price_range(self):
-        # первым делом зафиксируем места пропусков информации
-        self.final_data['Price_Range_isNAN'] = pd.isna(self.raw_data['Price Range']).astype('uint8')
+        # Для некоторых алгоритмов МЛ даже для не категориальных 
+        # признаков можно применить One-Hot Encoding, и это может улучшить качество модели. Пробуйте разные подходы к кодированию признака - никто не знает заранее, что может взлететь.
 
         def create_conditions_for_changes(data):
             if pd.isna(data):
@@ -83,42 +121,57 @@ class ConfigWrapper:
                 return 3
 
         self.final_data['Price Range'] = self.final_data['Price Range'].apply(create_conditions_for_changes)
-        # заполняем пропуски
-        self.final_data['Price Range'].fillna(0, inplace=True)
-
+        # заполняем пропуски срезним значение цен по городам
+        for idx in self.final_data[self.final_data['Price Range'].isnull()].index:
+            self.final_data.at[idx, 'Price Range'] = round(self.final_data['Price Range'][self.final_data['City'] == self.final_data['City'].iloc[idx]].mean())
 
     def processing_number_of_reviews(self):
-        # фиксирую места пропусков информации
-        self.final_data['Number_of_Reviews_isNAN'] = pd.isna(self.raw_data['Number of Reviews']).astype('uint8')
         # заполняем пропуск
         self.final_data['Number of Reviews'].fillna(0, inplace=True)
+        # удаляю столбец Number of Reviews
+        # self.final_data = self.final_data.drop(columns='Number of Reviews')
 
     def processing_reviews(self):
         # форматируем данные в удобные списки
         self.final_data['Reviews'] = self.final_data['Reviews']\
                                                 .apply(format_str_values_to_list)
-        # фиксирую места пропусков информации
-        self.final_data['Reviews_isNAN'] = pd.isna(self.raw_data['Reviews']).astype('uint8')
         # количество БОЛЬШИХ букв в 2х отзывах
         self.final_data['Caps_Reviews'] = self.final_data['Reviews'].apply(Reviews.find_number_of_caps)
         # серия состоящая из списков в которых 2 даты публикации 2х последний отзывов
         self.raw_data['Reviews_Date'] = self.final_data['Reviews'].apply(Reviews.get_date_from_reviews)
         # время в сутках между публикациями отзывов
         self.final_data['Span_Reviews'] = self.raw_data['Reviews_Date'].apply(Reviews.get_days_between_reviews)
-        # удаляю столбец Reviews
-        self.final_data = self.final_data.drop(columns='Reviews')
         # заполняем пропуск
         self.final_data['Span_Reviews'].fillna(0, inplace=True)
-        self.final_data['Caps_Reviews'].fillna(self.final_data['Caps_Reviews'].mean(), inplace=True)
+        self.final_data['Caps_Reviews'].fillna(round(self.final_data['Caps_Reviews'].mean()), inplace=True)
+        # удаляю столбец Reviews
+        self.final_data = self.final_data.drop(columns='Reviews')
 
     def processing_url_ta(self):
         # выделить уникальные части ссылки. Ссылка состоит из 2х частей: Название ресторана - Место
-        self.raw_data['Unique_URL_Part'] = self.raw_data['URL_TA'].apply(Url.get_unique_link_part)
+        # self.raw_data['Unique_URL_Part'] = self.raw_data['URL_TA'].apply(Url.get_unique_link_part)
         # удаляю столбец URL_TA
         self.final_data = self.final_data.drop(columns='URL_TA')
 
+    def processing_id_ta(self):
+        # удаляем дубли, которые имеют одинаковый id в базе даных
+        self.final_data = self.final_data.drop_duplicates(subset=['ID_TA'], keep='last')
+        # удаляю столбец ID_TA
+        self.final_data = self.final_data.drop(columns='ID_TA')
+
 
 class CuisineStyle:
+    def get_most_popular_cuisine(city_series):
+        cuisine_dict = {}
+        for list_ in city_series:
+            if list_ != None:
+                for elem in list_:
+                    if elem in cuisine_dict:
+                        cuisine_dict[elem] += 1
+                    else:
+                        cuisine_dict[elem] = 1
+        return [max(cuisine_dict, key=cuisine_dict.get)]
+
     def get_unique_values_cuisine_style(series):
         cuisine = set()
         for list_ in series:
@@ -166,4 +219,3 @@ if __name__ == '__main__':
     pd.set_option('display.max_columns', None)
     df = pd.read_csv('main_task.xls')
     df_obj = ConfigWrapper(df)
-
